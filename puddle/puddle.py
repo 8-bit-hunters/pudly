4368dcea054e5e387de6ce8921bcf171366319da
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-11-22 19:36:24 +0100

    :sparkles: Raise error if the downloaded file size doesn't match the total size

--- puddle/puddle.py
+++ puddle/puddle.py
@@ -21,7 +21,9 @@ def download(
     except requests.exceptions.RequestException as e:
         raise DownloadError from e
 
-    log.info(f"Start download from {response.url}")
+    total_size = int(response.headers.get("content-length", 0))
+
+    log.info(f"Start download from {response.url} ({total_size} bytes)")
 
     try:
         file_name = Path(_get_filename_from_response(response))
@@ -34,8 +36,16 @@ def download(
 
     log.debug(f"File will be saved as {file_name}")
     with file_name.open("wb") as file:
+        downloaded_size = 0
         for chunk in response.iter_content(chunk_size=10 * 1024):
+            downloaded_size += len(chunk)
+            log.debug(f"Downloaded {downloaded_size} Bytes")
             file.write(chunk)
+
+    if not _is_file_size_correct(file_name, total_size):
+        message = "File size corrupted"
+        raise DownloadError(message)
+
     log.info(f"Downloaded {file_name}")
     return Path(file_name)
 
@@ -50,3 +60,7 @@ def _get_filename_from_url(url: str) -> str:
 def _get_filename_from_response(response: requests.Response) -> str:
     content_disposition = response.headers["content-disposition"]
     return content_disposition.split("filename=")[1]
+
+
+def _is_file_size_correct(file: Path, size: int) -> bool:
+    return file.stat().st_size == size
